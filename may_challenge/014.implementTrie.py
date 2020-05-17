a1d98e6d0a69fb745f172aef0b86fbb91f74ca62
Trinity Nguyen <thn.trinity@gmail.com>
2020-05-16 23:39:04 -0700

    Day 14 solution

---
+++ may_challenge/014.implementTrie.py
@@ -0,0 +1,52 @@
+# ----------------------------------------------------------------------
+# Name:         Remove K Digits
+# Author(s):    Trinity Nguyen
+# ----------------------------------------------------------------------
+
+"""
+    Implement a trie with insert, search, and startsWith methods.
+
+    Example:
+    Trie trie = new Trie();
+
+    trie.insert("apple");
+    trie.search("apple");   // returns true
+    trie.search("app");     // returns false
+    trie.startsWith("app"); // returns true
+    trie.insert("app");   
+    trie.search("app");     // returns true
+
+    Note:
+    - You may assume that all inputs are consist of lowercase letters a-z.
+    - All inputs are guaranteed to be non-empty strings.
+"""
+
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+
+    def insert(self, word: str) -> None:
+        """
+        Inserts a word into the trie.
+        """
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the trie.
+        """
+
+    def startsWith(self, prefix: str) -> bool:
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        """
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
